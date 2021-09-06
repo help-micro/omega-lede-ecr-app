6c7922164ac1d49ff136a5b008ec5d11707d7f51
Help Micro Git Admin <github@help-micro.com.ua>
2021-09-06 15:27:01 +0300

    Add localtunnel

--- ecr-app/client.js
+++ ecr-app/client.js
@@ -1,5 +1,6 @@
 var extend = require('extend');
 var digestRequest = require('request-digest')('admin', '555555');
+var request = require('request-promise');
 
 /**
  * Client URL
@@ -37,6 +38,39 @@ exports.runRequest = function (uri, method, data, extraOptions) {
             extend(options, extraOptions);
         } 
         digestRequest.request(options, function (error, response, body) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                resolve(body);
+            }
+        });
+    });
+};
+
+/**
+ * Run HTTP request
+ * @param uri
+ * @param method
+ * @param data
+ * @param extraOptions
+ * @returns {Promise}
+ */
+exports.runRequestUnauthorized = function (uri, method, data, extraOptions) {
+    return new Promise(function (resolve, reject) {
+        var options = {
+            method: method,
+            host: url,
+            uri: uri,
+			encoding: null
+        };
+        if (data !== undefined) {
+            options.body = data;//JSON.stringify(data);
+        }
+        if (extraOptions !== undefined) { 
+            extend(options, extraOptions);
+        } 
+        request(options, function (error, response, body) {
             if (error) {
                 reject(error);
             }
--- ecr-app/index.js
+++ ecr-app/index.js
@@ -1,8 +1,18 @@
 var http = require('http'),
 	https = require('https'),
     httpProxy = require('http-proxy');
+	
+var session = require('express-session');
+var express = require("express");
+var app = express();
+var uuid = require('uuid');
+var cmd = require('node-cmd');
+
+var localtunnel = require('localtunnel');
 
 var common = require('./common');
+
+var client = require('./client');
 
 // CA certificates
 var rootCas = require('ssl-root-cas/latest').create();
@@ -40,19 +50,84 @@ proxy.on('error', function (err, req, res) {
   	res.end('Device is offline');
 });
 
+// Check if global address is available
+var isForwardActive = false;
+
+// Port to be forwarded
+var localTunnelPort = 5050;
+
+// Temporary username for the access from a global network
+var localTunnelUsername = uuid.v4().toString();
+
+// Temporary password for the access from a global network
+var localTunnelPassword = uuid.v4().toString();
+
+// Timeout interval to update current info about a localtunnel service
+var localTunnelInterval = 15000;
+
+// Endpoint API URL
+var localTunnelApiEndpoint = 'http://ecr.help-micro.com.ua/data/devices/set-access-url';
+
+// Endpoint API key
+var localTunnelApiKey = null;
+
+// Global address
+var localTunnelAddress = "";
+
+// Maximum number of attempts to notify the server about URL
+const MAX_ATTEMPTS = 3;
+
+// Number of attempts to notify the server about URL
+var localTunnelAttempts = 0;
+
+// Serial number of a device
+var serialNumber = null;
+
+// Set session
+app.use(session({
+    secret: uuid.v4().toString(),
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+// Login endpoint
+app.get('/login', function (req, res) {
+	console.log(req.session);
+  if (!req.query.username || !req.query.password) {
+    res.send('login failed');    
+  } else if(req.query.username === localTunnelUsername || req.query.password === localTunnelPassword) {
+    req.session.user = localTunnelPassword;
+    req.session.admin = true;
+    res.redirect('/');
+  }
+});
+ 
+
+app.use(function(req, res) {
+	
+	var isLocal = req.connection.remoteAddress.includes("127.0.0.1") ? false : true;
+	//console.log(req.connection.remoteAddress);
+	//console.log(req.session, isLocal);
+	if ((req.session && req.session.admin) || isLocal)
+		return proxy.web(req, res, { target: 'http://' + global.ip + ':80' });
+	  else
+		return res.sendStatus(401);
+	
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  
+});
+
 //
 // Create your custom server and just call `proxy.web()` to proxy
 // a web request to the target passed in the options
 // also you can use `proxy.ws()` to proxy a websockets request
 //
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  proxy.web(req, res, { target: 'http://' + global.ip + ':80' });
-});
+var server = http.createServer(app);
 
 //console.log("listening on port 5050")
-server.listen(5050);
+server.listen(localTunnelPort);
 
 //
 // Create a HTTP Proxy server with a HTTPS target
@@ -71,3 +146,96 @@ var serverProxy = http.createServer(function(req, res) {
 	});
 });
 serverProxy.listen(3000);
+
+// Notify a remote server about a global address
+function notifyAboutTunnel() {
+	
+	// If API key is set
+	if (localTunnelApiKey != null) {
+		
+		// If the server was not previously notified about new address
+		if (localTunnelAttempts < MAX_ATTEMPTS && !isForwardActive) {
+			
+			console.log('notify');
+			
+			// Set headers, options, and body
+			var headers = {
+				'Authorization': 'Bearer ' + localTunnelApiKey
+			};
+			var extraOptions = {
+				headers: headers,
+				json: true 
+			};
+			var body = {
+				serial: serialNumber,
+				url: localTunnelAddress
+			};
+			
+			// Run requests
+			client.runRequestUnauthorized(localTunnelApiEndpoint, 'POST', body, extraOptions).then(function (response) {
+				isForwardActive = true;
+				localTunnelAttempts = 0;
+			}, function (err) {
+				localTunnelAttempts += 1;
+			});
+		}
+		else {
+			localTunnelAttempts = 0;
+			isForwardActive = false;
+		}
+	}
+}
+
+// Get the serial number of a device
+function getSerialNumber() {
+	if (serialNumber == null) {
+		client.setUrl("http://" + common.ip);
+		client.runRequest('/cgi/state', 'GET', undefined, {json: true}).then(function (response) {
+			if ('serial' in response) {
+				serialNumber = response['serial'];
+			}
+		}, function (err) {  });
+	}
+}
+
+setInterval(function() {
+	getSerialNumber();
+}, localTunnelInterval);
+
+// Get API key and start the process of the retrieving of a global address 
+cmd.get(`uci get system.@system[0].api`, function (err, data, stderr) {
+	localTunnelApiKey = data.trim();
+	if (localTunnelApiKey.length > 0) {
+		console.log(localTunnelApiKey);
+		setInterval(function () {
+			try {
+				if (!isForwardActive) {
+					localtunnel(localTunnelPort, function(err, tunnel) {
+						if (err) { isForwardActive = false; localTunnelAddress = "-"; }
+
+						// the assigned public url for your tunnel
+						// i.e. https://abcdefgjhij.localtunnel.me
+						else {
+							localTunnelAddress = tunnel.url + "/login?username=" + localTunnelUsername + "&password=" + localTunnelPassword;
+							notifyAboutTunnel();
+						}
+					});  
+				}
+			}
+			catch (e) {
+				
+				isForwardActive = false;
+			}
+			notifyAboutTunnel();
+			
+		}, localTunnelInterval);
+	}
+});
+
+
+process.on('uncaughtException', function (err) {
+    isForwardActive = false;
+	localTunnelAddress = '-';
+});
+
+
